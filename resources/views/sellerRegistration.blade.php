10020205a11394ec40c94d74726304be36ec91d8
Giang <jun154607@gmail.com>
2020-08-12 15:49:04 +0700

    #####

--- resources/views/sellerRegistration.blade.php
+++ resources/views/sellerRegistration.blade.php
@@ -3,25 +3,18 @@
     <html>
     <head>
         <style>
-            p {
+            html,body {
                 position: absolute;
                 width: 536px;
                 height: 357px;
                 left: 32px;
                 top: 181px;
-            }
-            html,body{
-                /* Body 14 */
                 font-family: Inter;
                 font-style: normal;
                 font-weight: normal;
                 font-size: 14px;
                 line-height: 150%;
-
-                /* or 21px */
                 letter-spacing: -0.01em;
-
-                /* Shukshuk Black */
                 color: #222831;
             }
         </style>
@@ -29,6 +22,7 @@
      <body>
 <p>Your Shukshuk seller account has been registered and is pending verification.</p>
      <p> We are working to verify your account and will contact you if we need additional information. We do this to ensure the highest level of security standards for both the merchant and the customer.</p>
+     <p>Upon verification of your account, we will notify you via email and you may then be able to upload products onto our platform for transactions.</p>
      </body>
     </html>
 @endsection
