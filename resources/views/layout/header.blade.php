ab4df6c40fc1b40867445bb158374a6aa33f3ab1
Giang <jun154607@gmail.com>
2020-08-12 15:32:27 +0700

    #

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{asset('./favicon.ico')}}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User CRUD Laravel Demo</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <style>
+        h1{
+            position: absolute
+        ;
+            left: 13.86%
+        ;
+            right: 33.17%
+        ;
+            top: 0%
+        ;
+            bottom: 0%
+        ;
+
+            font-family: Inter
+        ;
+            font-style: normal
+        ;
+            font-weight: 500
+        ;
+            font-size: 24px
+        ;
+            line-height: 29px
+        ;
+            letter-spacing: -0.019em
+        ;
+            text-transform: lowercase
+        ;
+            color: #222831
+        ;
+        }
+    </style>
+</head>
+<body>
+<h1>Shukshuk</h1>
+</body>
+</html>
---
+++ resources/views/sellerRegistration.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.master')
+@section('content')
+    <html>
+    <head>
+        <style>
+            p{
+                position: absolute;
+                width: 536px;
+                height: 357px;
+                left: 32px;
+                top: 181px;
+
+                /* Body 14 */
+                font-family: Inter;
+                font-style: normal;
+                font-weight: normal;
+                font-size: 14px;
+                line-height: 150%;
+
+                /* or 21px */
+                letter-spacing: -0.01em;
+
+                /* Shukshuk Black */
+                color: #222831;
+            }
+        </style>
+    </head>
+     <body>
+<p>Your Shukshuk seller account has been registered and is pending verification.</p>
+     </body>
+    </html>
+@endsection
