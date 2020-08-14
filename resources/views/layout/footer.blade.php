83658475416a8269ba7850de4f116191c9f36726
Giang <jun154607@gmail.com>
2020-08-14 14:02:27 +0700

    Fix Blade Tempalte ,unfinish

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,23 +1,156 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Footer / Email -->
+<div class="component h-scale bottom instance" id="n3059_12">
+    <!-- Rectangle 344 -->
+    <div class="scale rectangle" id="nI3059_12-3050_34"></div>
+    <!-- PT. Lorem Ipsum Indonesia Sampoerna Strategic Tower Karet Semanggi, Jakarta Selatan -->
+    <div class="center bottom text" id="nI3059_12-3050_35">PT. Lorem Ipsum Indonesia<br>Sampoerna Strategic Tower<br>Karet Semanggi, Jakarta Selatan
+    </div>
+    <!-- © 2020 Shukshuk -->
+    <div class="h-scale bottom text" id="nI3059_12-3050_47">© 2020 Shukshuk</div>
+    <!-- Logo / Shukshuk / Dark / 24 -->
+    <div class="component left top instance" id="nI3059_12-3050_177">
+        <!-- Shukshuk -->
+        <div class="left center text" id="nI3059_12-3050_177-77_1895">Shukshuk</div>
+        <!-- Logo / Silk / Dark / 24 -->
+        <div class="center frame" id="nI3059_12-3050_177-77_1896">
+            <!-- Group -->
+            <div class="group" id="nI3059_12-3050_177-77_1897">
+                <!-- Vector -->
+                <img src="https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/9c00/eb02/502362ffcea0eb6a01f577f01c90ba5b" class="scale vector" id="nI3059_12-3050_177-77_1898" width="20" height="8">
+                <!-- Vector -->
+                <img src="https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/51a1/f9c1/e5cc3e0969bcbd0458c84b540f36ffb5" class="scale vector" id="nI3059_12-3050_177-77_1899" width="20" height="8"></div>
+        </div>
+    </div>
+</div>
+<!-- Please do not reply to this email. If you have any questions, reach us out at admin@shukshuk.com -->
+<div class="subsets left top text" id="n3059_13">
+    <span id="n3059_13-6">Please do not reply to this email. If you have any questions, reach us out at </span>
+    <span id="n3059_13-8">admin@shukshuk.com</span>
+</div>
+<style type="text/css">
+    /* Footer / Email */
+    #n3059_12 {
+        height: 180px;
+    }
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="{{asset('./favicon.ico')}}">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User CRUD Laravel Demo</title>
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
-</head>
+    /* Rectangle 344 */
+    #nI3059_12-3050_34 {
+        background: rgb(246, 248, 248) none repeat scroll 0% 0%;
+    }
 
-<body>
-@include('layout.header')
-<hr>
-@section('content') @show
-<hr>
-@include('layout.footer')
-</body>
+    /* PT. Lorem Ipsum Indonesia Sampoerna Strategic Tower Karet Semanggi, Jakarta Selatan */
+    #nI3059_12-3050_35 {
+        font-family: Inter;
+        font-size: 12px;
+        font-weight: 400;
+        letter-spacing: -0.12px;
+        color: rgb(78, 86, 97);
+        vertical-align: middle;
+        text-align: center;
+        justify-content: center;
+        width: 536px;
+        transform: translate(0px, -0.656px);
+        line-height: 15.6px;
+        height: 48px;
+        left: calc(50% - 268px);
+        bottom: 63px;
+    }
 
-</html>
+    /* © 2020 Shukshuk */
+    #nI3059_12-3050_47 {
+        font-family: Inter;
+        font-size: 12px;
+        font-weight: 400;
+        letter-spacing: -0.12px;
+        color: rgb(172, 180, 180);
+        vertical-align: middle;
+        text-align: center;
+        justify-content: center;
+        transform: translate(0px, -0.656px);
+        line-height: 15.6px;
+        height: 16px;
+        left: 41.3333%;
+        right: 41.3333%;
+        bottom: 23px;
+    }
+
+    /* Logo / Shukshuk / Dark / 24 */
+    #nI3059_12-3050_177 {
+        width: 138px;
+        height: 29px;
+        left: 231px;
+        top: 32px;
+    }
+
+    /* Shukshuk */
+    #nI3059_12-3050_177-77_1895 {
+        font-family: Inter;
+        font-size: 24px;
+        font-weight: 500;
+        letter-spacing: -0.456px;
+        color: rgb(34, 40, 49);
+        vertical-align: middle;
+        justify-content: flex-start;
+        width: 107px;
+        transform: translate(0px);
+        line-height: 28.125px;
+        height: 30px;
+        left: 32px;
+        top: calc(50% - 14.5px);
+    }
+
+    /* Logo / Silk / Dark / 24 */
+    #nI3059_12-3050_177-77_1896 {
+        width: 24px;
+        height: 24px;
+        overflow: hidden;
+        top: calc(50% - 12.5px);
+    }
+
+
+    /* Vector */
+    #nI3059_12-3050_177-77_1898 {
+        inset: 45.83% 8.33333% 20.83%;
+    }
+
+    /* Vector */
+    #nI3059_12-3050_177-77_1899 {
+        inset: 20.83% 8.33333% 45.83%;
+    }
+
+    /* Please do not reply to this email. If you have any questions, reach us out at admin@shukshuk.com */
+    #n3059_13 {
+        align-items: normal;
+        color: rgb(0, 0, 0);
+        vertical-align: middle;
+        justify-content: flex-start;
+        width: 536px;
+        transform: translate(0px, -1.96px);
+        line-height: 21px;
+        height: 42px;
+        left: 32px;
+        top: 570px;
+    }
+
+    /* Please do not reply to this email. If you have any questions, reach us out at admin@shukshuk.com Subset */
+    #n3059_13-6 {
+        font-family: Inter;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 21px;
+        letter-spacing: -0.14px;
+        align-items: normal;
+        color: rgb(175, 180, 185);
+    }
+
+    /* Please do not reply to this email. If you have any questions, reach us out at admin@shukshuk.com Subset */
+    #n3059_13-8 {
+        font-family: Inter;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 21px;
+        letter-spacing: -0.14px;
+        align-items: normal;
+        color: rgb(48, 182, 164);
+    }
+</style>
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,48 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Header / Email / Seller -->
+<div class="center top component" id="n3050_12">
+    <!-- Rectangle 345 -->
+    <div class="scale rectangle" id="n3050_13"></div>
+    <!-- Line 24 -->
+    <img src="https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/7ee2/26d7/3ee4e58c7254f0dc965d9628c8000d23" class="h-scale top line" id="n3050_25" width="536" height="0">
+    <!-- Logo / Shukshuk / Seller / 24 -->
+    <div class="component center top instance" id="n3050_153">
+        <!-- Shukshuk -->
+        <div class="scale text" id="nI3050_153-442_2437">Shukshuk</div>
+        <!-- Logo / Silk / Dark / 24 -->
+        <div class="scale frame" id="nI3050_153-442_2438">
+            <!-- Group -->
+            <div class="group" id="nI3050_153-442_2439">
+                <!-- Vector -->
+                <img src="https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/9c00/eb02/502362ffcea0eb6a01f577f01c90ba5b" class="scale vector" id="nI3050_153-442_2440" width="20" height="8">
+                <!-- Vector -->
+                <img src="https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/51a1/f9c1/e5cc3e0969bcbd0458c84b540f36ffb5" class="scale vector" id="nI3050_153-442_2441" width="20" height="8"></div>
+        </div>
+        <!-- Seller -->
+        <div class="scale text" id="nI3050_153-442_2443">Seller</div>
+    </div>
+</div>
+<style type="text/css">
+    /* Header / Email / Seller */
+    #n3050_12 {
+        width: 536px;
+        height: 96px;
+        left: calc(50% - 268px);
+    }
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="{{asset('./favicon.ico')}}">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User CRUD Laravel Demo</title>
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
-    <style>
-        h1{
-            position: absolute
-        ;
-            left: 13.86%
-        ;
-            right: 33.17%
-        ;
-            top: 0%
-        ;
-            bottom: 0%
-        ;
+    /* Line 24 */
+    #n3050_25 {
+        top: 96px;
+        height: 0px;
+    }
 
-            font-family: Inter
-        ;
-            font-style: normal
-        ;
-            font-weight: 500
-        ;
-            font-size: 24px
-        ;
-            line-height: 29px
-        ;
-            letter-spacing: -0.019em
-        ;
-            text-transform: lowercase
-        ;
-            color: #222831
-        ;
-        }
-    </style>
-</head>
-<body>
-<h1>Shukshuk</h1>
-</body>
-</html>
+    /* Logo / Shukshuk / Seller / 24 */
+    #n3050_153 {
+        width: 202px;
+        height: 30px;
+        left: calc(50% - 101px);
+        top: 33px;
+    }
+
+    /* Shukshuk */
+    #nI3050_153-442_2437 {
+        font-family: Inter;
+        font-size: 24px;
+        font-weight: 500;
+        letter-spacing: -0.456px;
+        color: rgb(34, 40, 49);
+        vertical-align: middle;
+        justify-content: flex-start;
+        transform: translate(0px);
+        line-height: 28.125px;
+        left: 13.8614%;
+        right: 33.1683%;
+    }
+
+    /* Logo / Silk / Dark / 24 */
+    #nI3050_153-442_2438 {
+        overflow: hidden;
+        right: 88.1188%;
+        top: 6.67%;
+        bottom: 13.33%;
+    }
+
+
+    /* Vector */
+    #nI3050_153-442_2440 {
+        inset: 45.83% 8.33333% 20.83%;
+    }
+
+    /* Vector */
+    #nI3050_153-442_2441 {
+        inset: 20.83% 8.33333% 45.83%;
+    }
+
+    /* Seller */
+    #nI3050_153-442_2443 {
+        font-family: Inter;
+        font-size: 24px;
+        font-weight: 500;
+        letter-spacing: -0.456px;
+        color: rgb(48, 182, 164);
+        vertical-align: middle;
+        justify-content: flex-start;
+        transform: translate(0px);
+        line-height: 28.125px;
+        left: 68.8119%;
+    }
+</style>
